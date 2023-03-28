2d4676d834d21350c04f8498ced7466afe140fd1
伊藤 洋也 <hito@pepabo.com>
2023-03-28 14:48:23 +0900

    add YAML test

--- tests/test_bot.py
+++ tests/test_bot.py
@@ -1,6 +1,7 @@
 import sys
 import os
 import json
+import yaml
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
@@ -9,6 +10,10 @@ import bot
 def test_json():
   with open("components/menu.json", "r", encoding="utf8") as file:
       block = json.load(file)
+
+def test_yaml():
+   with open('config.yaml', encoding="utf8") as file:
+      config = yaml.safe_load(file)
 
 def test_load_bot():
   assert True, 'only load test'
