82a6b3a637341a302fc79818275e6610778b1440
伊藤 洋也 <hito@pepabo.com>
2023-03-28 14:22:51 +0900

    add json test

--- tests/test_bot.py
+++ tests/test_bot.py
@@ -1,9 +1,14 @@
 import sys
 import os
+import json
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
 import bot
+
+def test_json():
+  with open("components/menu.json", "r", encoding="utf8") as file:
+      block = json.load(file)
 
 def test_load_bot():
   assert True, 'only load test'
